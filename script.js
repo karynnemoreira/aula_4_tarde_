bd2c71912c7ef0e2df6a8e1f575e7e07ef98a0d0
karynnemoreira <karynnemoreira@yahoo.com.br>
2023-01-18 15:28:46 -0300

    aula 4 turma da tarde

---
+++ script.js
@@ -0,0 +1,117 @@
+// console.log( 3 > 2) //true
+// console.log ( 3 < 2) //false
+
+//console.log(true && false)//false pq um valor é verdadeiro e o outro é falso
+
+//console.log (true && true)//true pq os dois valores são verdadeiros
+
+// let a = 3 > 2 //true
+// let b = 2 > 3 //false
+
+// if (a == true && b == false){
+// console.log(`As duas condições são verdadeiras`)
+// } else {
+//     console.log(`As informações não batem`)
+// }
+
+// let c = 4 > 2
+// let d = 15 > 12
+
+// if(c == false && d == true){
+// console.log("As duas condições são verdadeiras")
+// }else{
+//     console.log("AS informações não batem")
+// }
+
+//Exercício, criar uma condição que exiba uma mensagem no console se a cantora fizer aniversário em Dezembro ou Janeiro
+
+// let cantora = "Ariana Grande"
+// let mes = "Janeiro"
+
+
+// if((mes === "Dezembro") || (mes === "Janeiro")){
+// console.log( `${cantora} faz aniversaŕio em Dezembro ou Janeiro`  )
+// } else{
+//     console.log (`${cantora} faz aniversário em ${mes} `)
+// }
+
+
+// let idade = 20
+// let nome = "Bia"
+// let cidade = "Rio de Janeiro"
+// console.log(`Meu nome é ${nome}, tenho ${idade} anos e sou do ${cidade}`)
+
+//Se ela nasceu em março ou agosto
+// let nome = "Isa"
+// let _mes = "Julho"
+
+// if((_mes === "março") || (_mes === "agosto")){
+// console.log( `A ${nome} nasceu em março ou agosto`)
+// }else{
+// console.log(`Não, a ${nome} nasceu em ${_mes}`)
+// }
+
+
+/// OPERADORES DE ATRIBUIÇÃO
+
+// console.log(0)
+// console.log(2)
+// console.log(4)
+// console.log(6)
+// console.log(8)
+// console.log(10)
+
+// = atribuição X = Y
+// significado x é igual a y
+
+
+// //Concatenação
+// let n1 = '2'
+// let n2 = '4'
+
+// console.log(n1 + n2)
+
+// //Soma/Adição
+// let n1 = 2
+// let n2 = 4
+
+// console.log(n1 + n2)
+
+
+// += adição x+=y  
+//significado: X = X+Y 
+
+// -= subtração x-= Y
+//significado: X = X-Y
+
+// *= multiplicação x*=y
+//significado: x=x*y
+
+// /= divisão x/=y
+//significado: x= x/y
+
+
+let x = 5
+let y = 3
+
+//soma
+// console.log (x = x+y)//completo
+//console.log (x+=y) //simplificado
+
+//subtração
+//console.log (x = x-y) //completa
+//console.log (x-=y) //simplificada
+
+//multiplicação
+//console.log (x = x*y) //completa
+//console.log (x*=y)// simplificada
+
+//divisão
+//console.log (x = x/y) //completa
+//console.log (x/=y) //simplificada
+
+//incremento
+//se X é 5, então x+x, define 6
+
+//decremento
+//se X é 5, então x-x, define 4
